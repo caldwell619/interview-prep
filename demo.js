981cf4d659fd02e8cbafc2bcef86e7d427cd14b9
caldwell619 <caldwell8814@gmail.com>
2019-02-12 09:57:15 -0600

    refactored solution

--- demo.js
+++ demo.js
@@ -4,9 +4,37 @@ const minutes = [0,1,100,500,1000,1300,1500,4000,6000];
 
 // start with accepting one
 const printClockFormat = minuteInputs => {
-	let outputArray = [];
-	minuteInputs.forEach(input => {
-		// 60 minutes per hour
+	
+	let formattedResponse =  minuteInputs.map(input => {
+		
+		let day = determineDays(input)[0];
+
+		input = determineDays(input)[1];
+
+		let hours = Math.floor((input / 60));
+
+		let minutes = determineMinutes(input, hours);
+
+		let amPm = "AM";
+
+		// adding formatting and AM / PM	
+		if (hours < 10) {
+			hours = `0${hours}`
+		} else if (hours > 12){
+			amPm = "PM";
+			hours -= 12;
+		}
+
+		// change to increasing the day
+		return `${day} day past - time: ${hours}:${minutes}${amPm}`;
+		
+	})
+		
+	return formattedResponse;
+};
+
+// helper functions
+const determineDays = input => {
 	let day = 0;
 	// 1440 minutes per day
 	if (input >= 1440) {
@@ -14,34 +42,21 @@ const printClockFormat = minuteInputs => {
 		day += Math.floor( input / 1440);
 		input = (input / day - 1440);
 	}
-	// findout the hours and minutes individually
-	let hours = Math.floor((input / 60));
+	return [day, input]
+};
+
+const determineMinutes = (input, hours) => {
 	let minutes = ((input / 60).toFixed(2)) - hours;
-	minutes = Math.round(minutes * 60);
+	minutes = Math.floor(minutes * 60);
 	//adding formatting
 	if (minutes <= 10) {
 		minutes+="0"
 	}
-	let amPm = "";
-	let output = "";
+	return minutes;
+}
 
-	// adding formatting and AM / PM	
-	if (hours < 10) {
-		amPm = "AM";
-		hours = `0${hours}`
-	} else if (hours > 12){
-		amPm = "PM";
-		hours -= 12;
-		hours = `${hours}`
-	}
 
-	// change to increasing the day
-		output = `${day} day past - time: ${hours}:${minutes}${amPm}`;
-		outputArray.push(output);
-	})
-	
-	return outputArray;
-};
+
 
 
 console.log(printClockFormat(minutes));
